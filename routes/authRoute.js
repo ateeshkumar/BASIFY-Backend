92a8b025b30df22b6708e66750874844776691f6
Ateesh Kumar <88398308+ateeshkumar@users.noreply.github.com>
2024-06-07 14:38:29 +0530

    Login Crud Complete

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { userRegistrationController } = require('../controllers/authController');
+const { userRegistrationController, userLoginController } = require('../controllers/authController');
 
 const authRoute = express.Router();
 
 authRoute.post('/signup',userRegistrationController);
 
+authRoute.post("/login",userLoginController);
 
 module.exports = authRoute;
